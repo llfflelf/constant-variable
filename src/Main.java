9b7fc6478ff3e2cd47d3eae38e2b978e89f70ba7
llfflelf <dlehdtn48@daegu.ac.kr>
2025-06-02 10:42:35 +0900

    third commit

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ public class Main {
         int a = 10;
         int b = 3;
 
-        int sum = a + b;
-        System.out.println(sum);
+        a += 5;
+        System.out.println(a); // 15
 
-        int difference = a - b;
-        System.out.println(difference);
-        int product = a * b;
-        System.out.println(product);
-        int quotient = a / b;
-        System.out.println(quotient);
-        int remainder = a % b;
-        System.out.println(remainder);
+        b -= 1;
+        System.out.println(b); // 2
+        b *= 4;
+        System.out.println(b); // 8
+        b /= 3;
+        System.out.println(b); // 2
+        b %= 2;
+        System.out.println(b); // 0
     }
 }
